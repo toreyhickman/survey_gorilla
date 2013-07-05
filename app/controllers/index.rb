ff17862c1dbb28111f5b2e1284a3440476291a7e
Chris Jeon <chris0374@gmail.com>
2013-07-05 17:20:42 -0500

    register feature added with ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -20,7 +20,25 @@ post '/login' do
 	end
 end
 
+post '/register' do
+	user_info = params[:user_info]
+	user = User.create(username: user_info[:username], password: user_info[:password])
+	session[:user_id] = user.id
+	redirect '/'
+end
+
 get '/logout' do
 	session.clear
 	redirect '/'
+end
+
+post '/signform' do
+	@action = params[:action]
+	@submit_button = params[:button]
+
+	if request.xhr?
+		erb :_logform, layout: false
+	else
+		redirect '/'
+	end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+	$('#signin').on('click', function(event) {
+		event.preventDefault();
+		var submitButton = {action: '/login', button: 'Log in'}
+		$.post('/signform', submitButton, function(response) {
+			$('#signform').html(response);
+		});
+	});
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	$('#register').on('click', function(event) {
+		event.preventDefault();
+		var submitButton = {action: '/register', button: 'Register'}
+		$.post('/signform', submitButton, function(response) {
+			$('#signform').html(response);
+		});
+	});
 });
