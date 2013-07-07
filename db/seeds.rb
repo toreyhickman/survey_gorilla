43a24023f094eb270d6bd47adc9856fed2079461
Torey Hickman <torey@toreyhickman.com>
2013-07-07 16:16:09 -0500

    Rewrote seed file, higher resolution background image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,3 @@
-#lines 1-5 are for 5 questions per survey the rest is 1 question per survey
-
-# User.create(username: 'Rick',
-#             password: 'password')
-
-# User.create(username: 'Chris_L',
-#             password: 'password')
-
-# User.create(username: 'Chris_J',
-#             password: 'password')
-
-# User.create(username: 'Torey',
-#             password: 'password')
-
-# User.create(username: 'Karst',
-#             password: 'password')
-
-# 5.times do
-#   User.create(username: Faker::Internet.user_name, password: "password")
-# end
-
-# 15.times do
-#   Survey.create(name: Faker::Company.name, creator_id: rand(1..10))
-# end
-
-# count = 0
-# 15.times do
-# 	count += 1
-# 	5.times do
-#   	Question.create(text: Faker::Company.catch_phrase, survey_id: count) # puts 5 questions per survey
-#   end
-# end
-
-
-# count = 0
-# 45.times do
-# 	count += 1  #this makes sure that every question gets 5 options.
-# 	5.times do
-# 		Option.create(text: Faker::Company.bs, question_id: count)
-# 	end
-# end
-
-# 30.times do
-# 	Session.create(user_id: rand(1..5), survey_id: rand(1..15))
-# end
-
-# counter = 0
-# count = 0
-# 30.times do
-# 	count += 1
-# 	5.times do
-# 		counter += 1
-# 		Response.create(session_id: (count), opinion_id: ((counter * 5) - rand(0..4)))
-# 	end
-# end
-
-# _________________ above 5 questions per survey
-# 375 / 5 = 75
-# ---------------- Below one question per survey
-
 User.create(username: 'Rick',
             password: 'password')
 
@@ -73,39 +13,44 @@ User.create(username: 'Torey',
 User.create(username: 'Karst',
             password: 'password')
 
-5.times do
+users = User.all
+
+users.each do |user|
+  3.times do
+    survey = user.created_surveys.create(name: Faker::Company.name)
+    question = survey.questions.create(text: Faker::Company.catch_phrase)
+
+    5.times do
+      question.options.create(text: Faker::Company.bs)
+    end
+  end
+end
+
+15.times do
   User.create(username: Faker::Internet.user_name, password: "password")
 end
 
-15.times do
-  Survey.create(name: Faker::Company.name, creator_id: rand(1..10))
-end
+users = User.all
 
-count = 0
-15.times do
-	count += 1
-  Question.create(text: Faker::Company.catch_phrase, survey_id: count)
-end
+users.each do |user|
+  user_created_survey_ids = user.created_surveys.map { |survey| survey.id }
+
+  surveys_to_answer = Survey.all.select { |survey| !user_created_survey_ids.include?(survey.id) }
+  surveys_to_answer = surveys_to_answer.shuffle.values_at(* surveys_to_answer.each_index.select { |i| i.even? })
 
 
-count = 0
-15.times do
-	count += 1  #this makes sure that every question gets 5 options.
-	5.times do
-		Option.create(text: Faker::Company.bs, question_id: count)
-	end
-end
+  surveys_to_answer.each do |survey|
 
-puts "About to seed sessions .....\n\n\n\n\n\n\n\n\n"
+    session = Session.create(user_id: user.id, survey_id: survey.id)
 
-30.times do
-	Session.create(user_id: rand(1..5), survey_id: rand(1..15))
-end
+    questions = survey.questions
 
-counter = 0
-15.times do
-	counter += 1
-	2.times do
-		Response.create(session_id: counter, option_id: ((counter * 5) - rand(5)))
-	end
+    questions.each do |question|
+
+      option_selected = question.options.sample
+
+      Response.create(session_id: session.id, option_id: option_selected.id)
+
+    end
+  end
 end
